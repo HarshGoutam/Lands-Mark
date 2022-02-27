e5284f57ee437992434f2b76832b93d37dc2ba5e
Harsh Goutam <87484626+HarshGoutam@users.noreply.github.com>
2022-02-27 11:29:25 +0530

    Add files via upload

---
+++ Landsmarks/Hike.swift
@@ -0,0 +1,31 @@
+//
+//  Hike.swift
+//  Landsmarks
+//
+//  Created by Harsh Goutam on 05/12/21.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Harsh Goutam on 27/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
